d203a5a3a844eecabcb50f4d2c999cb1b7aa5844
Chaves.Rodrigo <rodrigoccavalcante@gmail.com>
2022-01-27 01:01:38 -0300

    add type:module to package.json

--- projeto_12.sh
+++ projeto_12.sh
@@ -4,7 +4,7 @@ REPLACE='exit 1",\n\t\t"start": "npx nodemon index.js'
 PROJECT='projeto_12_API_Batepapo_UOL'
 
 mkdir $PROJECT
-cd projeto_12_API_Batepapo_UOL
+cd $PROJECT
 
 git init &&
 npm init -y &&
@@ -13,11 +13,12 @@ npm i express &&
 npm i mongodb &&
 npm i cors &&
 npm i dotenv &&
-touch .gitignore &&
+touch .gitignore
+touch .env
+touch index.js
 echo '/node_modules' >> .gitignore &&
-touch .env &&
 echo '.env' >> .gitignore &&
-touch index.js &&
 sed -i "s/$SEARCH/$REPLACE/" package.json
+sed -i '/\"main\": \"index.js\",/a\\t\"type\": \"module\",' package.json
 code .
 #&& exit 1 não consigo fazer o terminal fechar
