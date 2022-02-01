de7bf1a70c22dd29ba91eb4229abdeef2e3c998e
Rodrigo Chaves <rodrigoccavalcante@gmail.com>
2022-02-01 16:56:49 -0300

    Create projeto_13_front.sh

---
+++ projeto_13_front.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+create-react-app projeto_13_mywallet_front &&
+cd projeto_13_mywallet_front &&
+npm i axios &&
+npm i react-router-dom &&
+npm i styled-components &&
+npm i dayjs &&
+code . &&
+npm start
